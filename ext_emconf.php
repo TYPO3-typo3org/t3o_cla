a319daf0602b78d681f76030643c1a04eb4a85bd
soren.malling <soren.malling@e8cb78a8-64f2-4011-8042-bd1ea5058d47>
2011-04-22 13:22:55 +0000

    Imported t3o_cla

---
+++ ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "t3o_cla".
+#
+# Auto generated 21-04-2011 12:55
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'typo3.org CLA Flag',
+	'description' => 'Adds a "has signed CLA" to each frontend user.',
+	'category' => 'fe',
+	'author' => 'Robert Lemke',
+	'author_email' => 'robert@typo3.org',
+	'shy' => '',
+	'dependencies' => 'cms',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"2dd8";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"6f2f";s:14:"ext_tables.sql";s:4:"81c3";s:16:"locallang_db.xml";s:4:"7fae";s:19:"doc/wizard_form.dat";s:4:"8905";s:20:"doc/wizard_form.html";s:4:"9c5c";}',
+	'suggests' => array(
+	),
+);
+
+?>
---
+++ ext_tables.sql
@@ -0,0 +1,6 @@
+#
+# Table structure for table 'fe_users'
+#
+CREATE TABLE fe_users (
+	tx_t3ocla_hassignedcla tinyint(3) DEFAULT '0' NOT NULL
+);
